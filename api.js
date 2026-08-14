0a6e70499fe669955172d593f2152e8ef5742ecb
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-08-14 10:51:29 +0200

    Cambios en api.js para mejora de testeo con Postman en los ejercicios

--- api.js
+++ api.js
@@ -4,19 +4,19 @@ const app = express()
 const user = require('./user.controller')
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', user.list)
-
 app.post('/', user.create)
-
 app.get('/:id', user.get)
-
 app.put('/:id', user.update)
-
 app.patch('/:id', user.update)
-
 app.delete('/:id', user.destroy)
 
-
+// 404 - siempre al final de las rutas
+app.use((req, res) => {
+  res.status(404).send('Ruta no encontrada')
+})
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor escuchando en http://localhost:${port}`)
